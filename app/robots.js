2346b945700e7861a16c0b5a3088ea37d506383f
A7iintelligence <salman@a7iintelligence.com>
2026-08-15 15:33:08 +1000

    Add files via upload

--- app/robots.js
+++ app/robots.js
@@ -1,1 +1,7 @@
-
+export default function robots() {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: "https://supplements.a7iintelligence.com/sitemap.xml",
+    host: "https://supplements.a7iintelligence.com",
+  };
+}
